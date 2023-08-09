cdafbaff51ac3e501b4b451afca0f16c4fc7abe1
cetrin2 <38029626+cetrin2@users.noreply.github.com>
2023-08-09 11:32:52 +0300

    Create lesson3.py

---
+++ lesson3.py
@@ -0,0 +1,26 @@
+
+#Задание 1
+print("Enter type of animal")
+animal_type = input()
+
+print("Enter age")
+age = input()
+
+print("Enter name")
+name = input()
+
+print("This is a ", animal_type,". Its name is ", name, "and age is ", age)
+print()
+
+#Задание 2
+print("Enter all stages of human development")
+
+stage1=input()
+stage2=input()
+stage3=input()
+stage4=input()
+stage5=input()
+stage6=input()
+
+print("Your answer is :")
+print(stage1, stage2, stage3, stage4, stage5, stage6, sep=" => ")
